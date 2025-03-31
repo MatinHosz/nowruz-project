7ad1b68db553c66b8072690f1609bae2899fe4f9
MatinHosz <matin.hosseinzadeh06@gmail.com>
2025-04-01 01:59:59 +0330

    Add example files
    for test codes and program

---
+++ NowruzProject/src/db/example/Human.java
@@ -0,0 +1,19 @@
+package db.example;
+
+import db.Entity;
+
+public class Human extends Entity {
+    public String name;
+    public int age;
+
+    public Human(String name) {
+        this.name = name;
+    }
+    @Override
+    public Human copy() {
+        Human copyHuman = new Human(name);
+        copyHuman.id = id;
+
+        return copyHuman;
+    }
+}
