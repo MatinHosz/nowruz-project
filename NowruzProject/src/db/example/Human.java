4227ec552c6644e1a1fae568ff9087b8cd137772
MatinHosz <matin.hosseinzadeh06@gmail.com>
2025-04-01 02:07:22 +0330

    add copy filed and adapt class parts to it

--- NowruzProject/src/db/example/Human.java
+++ NowruzProject/src/db/example/Human.java
@@ -6,13 +6,15 @@ public class Human extends Entity {
     public String name;
     public int age;
 
-    public Human(String name) {
+    public Human(String name, int age) {
         this.name = name;
+        this.age = age;
     }
     @Override
     public Human copy() {
         Human copyHuman = new Human(name);
         copyHuman.id = id;
+        copyHuman.age = age;
 
         return copyHuman;
     }
