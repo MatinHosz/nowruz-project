38b37cd8dfb7997915d90685cc36276936167398
MatinHosz <matin.hosseinzadeh06@gmail.com>
2025-04-01 12:53:30 +0330

    Create human validator

---
+++ NowruzProject/src/db/example/HumanValidator.java
@@ -0,0 +1,17 @@
+package db.example;
+
+import db.Entity;
+import db.validator;
+
+public class HumanValidator implements Validator {
+    @Override
+    public void validate(Entity entity) throws InvalidEntityException {
+        if (!(entity instanceof Human))
+            throw new IllegalArgumentException("Invalid input type. Expected Human");
+        if (((Human) entity).age < 0 || ((Human) entity).name.isEmpty() || ((Human) entity).name == null)
+            throw new InvalidEntityException("Invalid Human entity: Age cannot be negative");
+        if (((Human) entity).name.isEmpty() || ((Human) entity).name == null)
+            throw new InvalidEntityException("Invalid Human entity: Name cannot be null or empty");
+    }
+
+}
