e7970843c5f2dd7e8d4cd7178bab035ed07a1ee9
MatinHosz <matin.hosseinzadeh06@gmail.com>
2025-04-06 10:07:39 +0330

    Create document class

---
+++ NowruzProject/src/db/example/Document.java
@@ -0,0 +1,31 @@
+package db.example;
+
+import db.Entity;
+import db.Trackable;
+
+import java.util.Date;
+
+public class Document extends Entity implements Trackable {
+    public String content;
+    public Date creationDate;
+    public Date lastModificationDate;
+
+    public Document (int id, String content) {
+        super(id);
+        this.content = content;
+    }
+
+    public void setCreationDate(Date date) {
+        creationDate = date;
+    }
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setLastModificationDate(Date date) {
+        lastModificationDate = date;
+    }
+    public Date getLastModificationDate() {
+        return lastModificationDate;
+    }
+}
