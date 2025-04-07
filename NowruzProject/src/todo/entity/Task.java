e94480ec3b1338cf7719bab6f91e312bdc043724
MatinHosz <matin.hosseinzadeh06@gmail.com>
2025-04-07 19:58:27 +0330

    Create Task class

---
+++ NowruzProject/src/todo/entity/Task.java
@@ -0,0 +1,55 @@
+package todo.entity;
+
+import db.Entity;
+import db.Trackable;
+
+import java.util.Date;
+
+public class Task extends Entity implements Trackable {
+    public static final int TASK_ENTITY_CODE = 2;
+
+    enum Status{
+        NotStarted,
+        InProgress,
+        Completed
+    }
+
+    public String title;
+    public String description;
+    private Date creationDate;
+    private Date lastModificationDate;
+    public Date dueDate;
+    public Status status;
+
+    public Task(String title, String description,) {
+        //TODO complete constructor
+    }
+
+    @Override
+    public Date getCreationDate() {
+        return creationDate;
+    }
+    @Override
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    @Override
+    public Date getLastModificationDate() {
+        return lastModificationDate;
+    }
+    @Override
+    public void setLastModificationDate(Date lastModificationDate) {
+        this.lastModificationDate = lastModificationDate;
+    }
+
+    @Override
+    public int getEntityCode() {
+        return TASK_ENTITY_CODE;
+    }
+    @Override
+    public Task copy() {
+        //TODO complete copy method
+        Task copyTask = new Task()
+    }
+}
