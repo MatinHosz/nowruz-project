496cf0a2eccce58c9d4a076242b35d25f6ce989f
MatinHosz <matin.hosseinzadeh06@gmail.com>
2025-04-07 21:58:59 +0330

    Add methods to update task status to NotStarted and InProgress in TaskService

--- NowruzProject/src/todo/service/TaskService.java
+++ NowruzProject/src/todo/service/TaskService.java
@@ -5,9 +5,22 @@ import db.exception.InvalidEntityException;
 import todo.entity.Task;
 
 public class TaskService {
+
     public static void setAsCompleted(int taskId) throws InvalidEntityException {
         Task task = (Task) Database.get(taskId);
         task.status = Task.Status.Completed;
         Database.update(task);
     }
+    public static void setAsNotStarted(int taskId) throws InvalidEntityException {
+        Task task = (Task) Database.get(taskId);
+        task.status = Task.Status.NotStarted;
+        Database.update(task);
+    }
+    public static void setAsInProgress(int taskId) throws InvalidEntityException {
+        Task task = (Task) Database.get(taskId);
+        task.status = Task.Status.InProgress;
+        Database.update(task);
+    }
+
+
 }
