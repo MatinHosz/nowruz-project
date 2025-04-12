557a8a56ec576a28d3734a6b2bdcee25130eaf48
MatinHosz <matin.hosseinzadeh06@gmail.com>
2025-04-12 21:44:44 +0330

    Add and edit StepService methods based on needs of todolist app

--- NowruzProject/src/todo/service/StepService.java
+++ NowruzProject/src/todo/service/StepService.java
@@ -1,26 +1,42 @@
 package todo.service;
 
+import java.util.ArrayList;
+
 import db.Database;
-import db.Entity;
 import db.exception.InvalidEntityException;
 import todo.entity.Step;
-
-import javax.xml.crypto.Data;
+import todo.entity.Task;
 
 public class StepService {
     public static void saveStep(int taskRef, String title) throws InvalidEntityException {
         Step step = new Step(title, taskRef);
-        step.status = Step.Status.NotStarted;
+        step.status = Step.Status.NOT_STARTED;
         Database.add(step);
     }
     public static void setAsNotStarted(int stepId) throws InvalidEntityException {
         Step step = (Step) Database.get(stepId);
-        step.status = Step.Status.NotStarted;
+        step.status = Step.Status.NOT_STARTED;
         Database.update(step);
     }
     public static void setAsCompleted(int stepId) throws InvalidEntityException {
         Step step = (Step) Database.get(stepId);
-        step.status = Step.Status.Completed;
+        step.status = Step.Status.COMPLETED;
+
+         Task taskRefOfStep = (Task) Database.get(step.taskRef);
+         if (taskRefOfStep.status == Task.Status.NOT_STARTED)
+             taskRefOfStep.status = Task.Status.IN_PROGRESS;
+
+        ArrayList<Step> refTasksSteps = Database.getTasksSteps(step.taskRef);
+        boolean allStepsCompleted = true;
+        for (Step tasksStep: refTasksSteps) {
+            if (tasksStep.status != Step.Status.COMPLETED) {
+                allStepsCompleted = false;
+                break;
+            }
+        }
+        if (allStepsCompleted)
+            taskRefOfStep.status = Task.Status.COMPLETED;
+        
         Database.update(step);
     }
     public static void updateTitle(int id, String title) throws InvalidEntityException {
